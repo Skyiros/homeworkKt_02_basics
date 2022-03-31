58f5d3933643c052639bc6b28b840ac7d01d4979
Skyiros <sherst155@mail.ru>
2022-04-01 00:10:50 +0300

    Fixed the ending selection error

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val likes: Int = 1132
+    val likes: Int = 11
 
-    if (likes == 1 || (likes % 10) == 1 && likes != 11)
+    if (likes == 1 || (likes % 10) == 1 && likes % 100 != 11)
         println("Понравилось $likes человеку")
     else if (likes >= 100 && (likes % 100) == 0)
         println("Понравилось $likes человек")
